a21f301f7159aa25413b8f2e32f2a1368340e098
jasminb <jasminb@JASMIN>
2018-05-27 03:05:39 +0200

    Updated SQL query

--- hms.api/sql/hms.sql
+++ hms.api/sql/hms.sql
@@ -1,20 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.5.1
--- http://www.phpmyadmin.net
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 22, 2018 at 01:05 PM
--- Server version: 5.7.11
--- PHP Version: 5.6.19
+-- Generation Time: May 27, 2018 at 01:05 AM
+-- Server version: 5.7.14
+-- PHP Version: 7.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `hms`
@@ -69,7 +63,8 @@ CREATE TABLE `room` (
   `room_id` int(11) NOT NULL,
   `door_number` int(11) DEFAULT NULL,
   `room_category_id` int(11) DEFAULT NULL,
-  `capacity` int(11) DEFAULT NULL,
+  `capacity_adults` int(11) DEFAULT NULL,
+  `capacity_children` int(11) DEFAULT NULL,
   `comment` text
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
@@ -81,7 +76,7 @@ CREATE TABLE `room` (
 
 CREATE TABLE `room_category` (
   `room_category_id` int(11) NOT NULL,
-  `name` int(11) DEFAULT NULL
+  `name` varchar(11) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -103,6 +98,10 @@ CREATE TABLE `user` (
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `comment` text
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
 
 --
 -- Indexes for table `guest`
@@ -147,22 +146,19 @@ ALTER TABLE `guest`
 -- AUTO_INCREMENT for table `role`
 --
 ALTER TABLE `role`
-  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `room`
 --
 ALTER TABLE `room`
-  MODIFY `room_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `room_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT for table `room_category`
 --
 ALTER TABLE `room_category`
-  MODIFY `room_category_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `room_category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
